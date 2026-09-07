ee16b89cedf079ad19ff4e5d5a7b6916d3802027
Juan Diego <chucjuan508@gmail.com>
2026-09-07 16:44:51 -0600

    se agrego el metodo para min heap, asi mismo se dividio el trabajo para trabajar modularmente, programa aun no terminado pendiente de pruebas 100% reales

--- obj/Debug/net10.0/proyecto_1_ED2.AssemblyInfo.cs
+++ obj/Debug/net10.0/proyecto_1_ED2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto_1_ED2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0231e5da95953d80adee4398a692c446b70a331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34a31f59519041dc26a4ff4b10acae433bfdeb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto_1_ED2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto_1_ED2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
