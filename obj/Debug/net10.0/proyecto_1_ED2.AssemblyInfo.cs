7de72319fd706c25e5e445e2722e7dbb491819b0
Juan Diego <chucjuan508@gmail.com>
2026-09-08 10:40:14 -0600

    prueba

--- obj/Debug/net10.0/proyecto_1_ED2.AssemblyInfo.cs
+++ obj/Debug/net10.0/proyecto_1_ED2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto_1_ED2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7832d952f508dc0f7fe22521e0db7cc046c7c332")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e505f6488323ec14003ca704884f0b2b6ab5232e")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto_1_ED2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto_1_ED2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
